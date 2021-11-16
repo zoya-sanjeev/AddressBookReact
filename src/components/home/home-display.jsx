1a659cab1419ab6caca2fdb0b51e934d13bad1ca
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-16 21:45:30 +0530

    ADDED: display for table along with its styling

---
+++ src/components/home/home-display.jsx
@@ -0,0 +1,38 @@
+import '../home/home.scss'
+import { Link } from 'react-router-dom'
+const Display = (props) => {
+    return (
+        <table id="display" className="table">
+        <tbody>
+            <tr key={-1}>
+                <th>Name</th>
+                <th>Address</th>
+                <th>City</th>
+                <th>State</th>
+                <th>ZipCode</th>
+                <th>Phone Number</th>
+                <th></th>
+            </tr>
+            {
+                props.contactArray && props.contactArray.map((contact, index) => (
+                    <tr key={index}>
+                        <td>{contact.name}</td>
+                        <td>{contact.address}</td>
+                        <td>{contact.city}</td>
+                        <td>{contact.state}</td>
+                        <td>{contact.zip}</td>
+                        <td>{contact.phoneNumber}</td>
+                        <td>
+                            <img onClick={() => remove(contact.id)} alt="delete" src={deleteIcon} />
+                            <Link to={`/update/${contact.id}`} > 
+                                <img src={editIcon} alt="edit" /> 
+                            </Link>
+                        </td>
+                    </tr>
+                ))
+            }
+        </tbody>
+    </table>
+    );
+}
+export default Display;
