dedfdb4a3b9784bc6e7274a0706c5f2c07d8b3c2
Zoya Sanjeev <zoyasanjeev99@gmail.com>
2021-11-17 22:58:00 +0530

    MODIFIED: addresbook services to make api calls using rest

--- src/services/AddressBookService.js
+++ src/services/AddressBookService.js
@@ -4,18 +4,18 @@ import AxiosService from './axios-service'
 export default class AddressBookService{
     baseUrl = config.baseUrl;
     addAddressBookData(data){
-        return AxiosService.postService(`${this.baseUrl}AddressBookDB`, data);
+        return AxiosService.postService(`${this.baseUrl}create/`, data);
     }
     getAllContacts() {
-        return AxiosService.getService(`${this.baseUrl}AddressBookDB`);
+        return AxiosService.getService(`${this.baseUrl}`);
     }
     getContact(id) {
-        return AxiosService.getService(`${this.baseUrl}AddressBookDB/${id}`);
+        return AxiosService.getService(`${this.baseUrl}AddressBookDB/get/`,id);
     }
     updateContact(data, id) {
-        return AxiosService.putService(`${this.baseUrl}AddressBookDB/${id}`, data);
+        return AxiosService.putService(`${this.baseUrl}AddressBookDB/update/`,id, data);
     }
     deleteContact(id) {
-        return AxiosService.deleteService(`${this.baseUrl}AddressBookDB/${id}`);
+        return AxiosService.deleteService(`${this.baseUrl}AddressBookDB/delete/`, id);
     }
 }
